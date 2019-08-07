39828941f8e67df73ee06c4c98160eaadeebea8c
chinmay17 <chaudharychinmay@gmail.com>
2019-08-07 18:32:43 +0530

    Updates

---
+++ precache-manifest.8c59ec65dcae826ac1ba0b3b5ef5f4d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f03516709d88ebed439c41df589da3e5",
+    "url": "/react-virtualized-flowchart/index.html"
+  },
+  {
+    "revision": "4e4bc3743720dae32e2a",
+    "url": "/react-virtualized-flowchart/static/css/main.209fca48.chunk.css"
+  },
+  {
+    "revision": "9a829dd91f9c52c418cc",
+    "url": "/react-virtualized-flowchart/static/js/2.3559408b.chunk.js"
+  },
+  {
+    "revision": "4e4bc3743720dae32e2a",
+    "url": "/react-virtualized-flowchart/static/js/main.39462996.chunk.js"
+  },
+  {
+    "revision": "54ffd0754644675a1c44",
+    "url": "/react-virtualized-flowchart/static/js/runtime~main.8aff4c8b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-virtualized-flowchart/precache-manifest.5bd3af69b46efd120db71241ce530976.js"
+  "/react-virtualized-flowchart/precache-manifest.8c59ec65dcae826ac1ba0b3b5ef5f4d3.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-virtualized-flowchart/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-virtualized-flowchart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
